ea9c7008e3255e626aaa8c170297ed8fd459bc2d
unknown <fabrizioivan36@gmail.com>
2021-02-24 22:13:40 -0600

    Invert command added

---
+++ commands/fun/invert.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const canvacord = require("canvacord");
+
+module.exports = {
+  name: "invert",
+  aliases: [],
+  description: "Invert a image",
+  category: "Fun",
+  cooldown: 5,
+  run: async (client, message, args) => {
+    
+    let persona = message.mentions.users.first() || message.author;
+
+    let avatar = persona.displayAvatarURL({dynamic:false, format:'png', size:2048});
+
+    let datos = await canvacord.Canvas.invert(avatar);
+
+    let imagen = new Discord.MessageAttachment(datos,"InvertHelperBot.png");
+
+    message.channel.send(imagen)
+  }
+};
